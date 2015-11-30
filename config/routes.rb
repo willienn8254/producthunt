d359a028e55595b0c97954b597c7e04ecd08effd
willienn8254 <willienn@hotmail.com>
2015-11-29 19:40:00 -0500

    agrega root

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
     #patch '/products/:id', to: 'products#update'
    # delete '/products/:id', to: 'products#destroy'
    resources :products
+   root 'products#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
